641c1b80dbab4c6168412b5cf00052812d58bc0f
非幡动 <438484791@qq.com>
2020-07-06 19:51:56 +0800

    ES6入门

---
+++ 4/js/index.js
@@ -0,0 +1,19 @@
+// import * as mod1 from './mod1';
+
+// console.log(mod1.a,mod1.b,mod1.c);
+
+
+
+// import mod1 from './mod1';//只引default
+// //import default as mod1 from './mod1.js'即他的简写
+// console.log(mod1);
+
+
+
+// import * as mod1 from './mod1';
+// console.log(mod1);
+
+
+let b = 8;
+import {a,b as name2} from './mod1';
+console.log(a,b,name2);
---
+++ 4/js/mod1.js
@@ -0,0 +1,18 @@
+// export let a=12;
+// export let b=5;
+// export let c=6;
+
+
+
+// export default 99;
+
+
+
+// let [a,b,c]=[12,6,8];
+// export {a,b,c};
+
+
+
+export let a=12;
+export let b=5;
+export let c=6;
